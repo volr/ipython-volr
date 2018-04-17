7f2d8ce410e7f28b3004565ba947a4f165e2ee4d
Jens Egholm <jensegholm@protonmail.com>
2018-04-17 17:02:29 +0200

    Added debug mode

--- src/volr/magic.py
+++ src/volr/magic.py
@@ -3,41 +3,62 @@ from IPython.core.magic import (Magics, magics_class, line_magic, cell_magic)
 import re
 import subprocess
 import json
+import numpy as np
 from subprocess import PIPE
+
+class ParseException(Exception):
+    pass
+
+class ExecutionException(Exception):
+    pass
 
 @magics_class
 class VolrMagic(Magics):
 
-    json_pattern = "({.*})"
+    json_pattern = "(\[\[.*\]\])"
 
     @cell_magic('volr')
     def execute(self, line, cell):
-        content = cell
-        myelin_json, errors = self.process_script(content)
-        if errors:
-            return myelin_json + "\nProcess timed out: " + errors
+        return self.process_volr(cell, line)
 
-        process = subprocess.Popen(line.split(" "), stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    @cell_magic('volr-debug')
+    def execute(self, line, cell):
+        return self.process_volr(cell, line, debug = True)
+
+    def process_volr(self, volr, command, debug = False):
+        myelin_json = self.parse_volr(volr)
+        result = self.send_to_backend(myelin_json, command)
+
+        if (debug):
+            print(result)
+
+        json_string = re.search(self.json_pattern, result.decode()).group(1)
+        return np.array(json.loads(json_string))
+
+    def send_to_backend(self, myelin_json, command):
+        process = subprocess.Popen(command.split(" "), stdin=PIPE, stdout=PIPE, stderr=PIPE)
         try:
             result, errors = process.communicate(input = myelin_json)
         except subprocess.TimeoutExpired:
             result, errors = process.communicate()
 
         if errors:
-            return str(result) + "\n" + str(errors)
+            raise ExecutionException(str(result) + "\n" + str(errors))
 
-        json_string = re.search(self.json_pattern, result.decode()).group(1)
+        return result
 
-        return json.loads(json_string)
-
-    def process_script(self, content):
+    def parse_volr(self, content):
         process = subprocess.Popen(["volr"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
         try:
             result, errors = process.communicate(input = content.encode())
         except subprocess.TimeoutExpired:
             result, errors = process.communicate()
 
-        return result, errors
+        if errors:
+            raise ParseException(content + "\nProcess timed out: " + errors.decode())
+
+        return result
+
 
 def load_ipython_extension(ipython):
     """Load the extension into IPython"""
